a0e44abf3a864c3179ec280646ae6ac75bb04bf6
Brad <broswen@gmail.com>
2014-02-23 11:02:11 -0600

    added basic code

---
+++ src/me/broswen/bcontrol/BControl.java
@@ -0,0 +1,21 @@
+package me.broswen.bcontrol;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BControl extends JavaPlugin{
+	public static BControl plugin;
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
+		return true;
+	}
+}
